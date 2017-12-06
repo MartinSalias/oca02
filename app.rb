0632e8cec12658d940a4d8ec6dbacde639904ee1
vagrant <vagrant@kleer-csd-box>
2017-12-06 12:48:26 -0300

    pantalla bienvenida

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
+require "./lib/oca02.rb"
 
 get '/' do
     erb :iniciopartida
 end
 
+get '/jugar' do	
+		@@game = Uno.new
+		@@puntajeA = @@game.cerearjugadorA
+		@@puntajeB = @@game.cerearjugadorB
+    erb :pantallajuego
+end
+
+get '/tirardado' do	
+		@@resultado = @@game.tirardado
+    erb :pantallajuego
+end
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,4 +6,12 @@ Then(/^Veo "(.*?)"$/) do |titulo|
   last_response.body.should =~ /#{titulo}/m
 end
 
+When(/^Tirar Dado$/) do
+  click_button("Tirar Dado")
+end
+
+Then(/^Veo en campo resultado "(.*?)"$/) do |resultado|
+  last_response.body.should =~ /#{resultado}/m
+end
+
 
